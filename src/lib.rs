258bb124c7fd73aee7c3dee4d43b34d13c25eac8
Ubuntu <ubuntu@ip-172-31-22-57.us-east-2.compute.internal>
2020-04-09 05:00:54 +0000

    Started adding opcodes

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,17 @@
 use std::fmt;
 
-const INSTRUCTIONS_START: usize = 0x200;
+const INSTRUCTIONS_START: u16 = 0x200;
 
 pub struct Chip8 {
     // 4k of RAM
     ram: [u8; 4096],
 
-    //stack: [u16; 16],
+    stack: Vec<u16>,
 
     // registers
-    //v: [u8; 16],
-    //i: u16,
-    //sp: u16,
-    pc: u16,
+    v: [u8; 16],  // gen purpose
+    i: u16,       // index/address
+    pc: u16,      // program counter
 
     // state of keys
     //keys: [bool; 16],
@@ -28,20 +27,74 @@ impl Chip8 {
         let mut ram = [0; 4096];
         // TODO: verify rom length < ram length - 0x200
         for i in 0..rom.len() {
-            ram[i + INSTRUCTIONS_START] = rom[i];
+            let location = i + (INSTRUCTIONS_START as usize);
+            ram[location] = rom[i];
         }
         Chip8 {
             ram,
             //draw_flag: true,
-            //stack: [0; 16],
-            //v: [0; 16],
-            //i: 0,
+            stack: Vec::new(),
+            v: [0; 16],
+            i: 0,
             //sp: 0,
-            pc: INSTRUCTIONS_START as u16,
+            pc: INSTRUCTIONS_START,
             //keys: [false; 16],
             //delay_timer: 0,
             //sound_timer: 0,
         }
+    }
+
+    pub fn emulate_cycle(&mut self) {
+        let instr = self.fetch();
+        println!("Instruction: {}", instr);
+        match (instr & 0xf000) >> 12 {
+            0x2 => self.jump_subroutine(instr),
+            0x6 => self.set_register(instr),
+            0xa => self.set_index(instr),
+            _ => panic!("unrecognized instruction!"),
+        }
+    }
+
+    fn fetch(&self) -> u16 {
+        self.fetch_at(self.pc)
+    }
+
+    fn fetch_at(&self, addr: u16) -> u16 {
+        let addr = addr as usize;
+        let first_byte = self.ram[addr] as u16;
+        let second_byte = self.ram[addr + 1] as u16;
+        first_byte << 8 | second_byte
+    }
+
+    // Opcodes
+    fn jump_subroutine(&mut self, instr: u16) {
+        self.stack.push(self.pc);
+        self.pc = instr & 0x0fff;
+
+        println!("jumped to subroutine at {}", self.pc);
+    }
+
+    fn set_index(&mut self, instr: u16) {
+        // set the "I" register (index/address register)
+        let value = instr & 0x0fff;
+        self.i = value;
+
+        println!("set I to {}", self.i);
+
+        self.pc = self.pc + 2;
+    }
+
+    fn set_register(&mut self, instr: u16) {
+        // set a general purpose register (one of the "V's")
+        let reg = (instr & 0x0f00) >> 8;
+        let reg = reg as usize;
+        let value = (instr & 0x00ff) as u8;
+
+        self.v[reg] = value;
+
+        println!("Set V{} to {}", reg, self.v[reg]);
+
+        self.pc = self.pc + 2;
     }
 }
 
@@ -52,6 +105,8 @@ impl fmt::Display for Chip8 {
 }
 
 pub fn run(rom: Vec<u8>) {
-    let chip8 = Chip8::initialize(rom);
-    println!("{}", chip8);
+    let mut chip8 = Chip8::initialize(rom);
+    loop {
+        chip8.emulate_cycle();
+    }
 }
