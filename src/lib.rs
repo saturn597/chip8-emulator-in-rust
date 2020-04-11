93b6182fd5351fa60195d088d857304a8aab7600
Ubuntu <ubuntu@ip-172-31-22-57.us-east-2.compute.internal>
2020-04-11 19:33:44 +0000

    Add draw_sprite opcode

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,29 @@ use std::fmt;
 
 const INSTRUCTIONS_START: u16 = 0x200;
 
+#[derive(Copy,Clone,PartialEq)]
+enum Pixel {
+    On,
+    Off,
+}
+
+impl Pixel {
+    fn flip(&self) -> Pixel {
+        if *self == Pixel::On {
+            Pixel::Off
+        } else {
+            Pixel::On
+        }
+    }
+}
+
 pub struct Chip8 {
     // 4k of RAM
     ram: [u8; 4096],
 
     stack: Vec<u16>,
+
+    pixels: [[Pixel; 32]; 64],
 
     // registers
     v: [u8; 16],  // gen purpose
@@ -19,7 +37,7 @@ pub struct Chip8 {
     //delay_timer: u8,
     //sound_timer: u8,
 
-    //draw_flag: bool,
+    draw_flag: bool,
 }
 
 impl Chip8 {
@@ -32,8 +50,9 @@ impl Chip8 {
         }
         Chip8 {
             ram,
-            //draw_flag: true,
+            draw_flag: true,
             stack: Vec::new(),
+            pixels: [[Pixel::Off; 32]; 64],
             v: [0; 16],
             i: 0,
             //sp: 0,
@@ -51,6 +70,7 @@ impl Chip8 {
             0x2 => self.jump_subroutine(instr),
             0x6 => self.set_register(instr),
             0xa => self.set_index(instr),
+            0xd => self.draw_sprite(instr),
             _ => panic!("unrecognized instruction!"),
         }
     }
@@ -66,7 +86,53 @@ impl Chip8 {
         first_byte << 8 | second_byte
     }
 
+    fn show(&self) {
+        for y in 0..32 {
+            for x in 0..64 {
+                print!("{}", self.pixels[x][y]);
+            }
+            println!("");
+        }
+    }
+
     // Opcodes
+    fn draw_sprite(&mut self, instr: u16) {
+        let instr = instr as usize;
+
+        let x_reg = (instr & 0x0f00) >> 8;
+        let y_reg = (instr & 0x00f0) >> 4;
+
+        let n = instr & 0x000f;
+
+        let x_start = self.v[x_reg] as usize;
+        let y_start = self.v[y_reg] as usize;
+
+        let mem_start = self.i as usize;
+
+        let mut collision = false;
+
+        for i in 0..n {
+            let mem_location = mem_start + i;
+            let byte = self.ram[mem_location];
+            for j in (0..8).rev() {
+                let x = x_start + j;
+                let y = y_start + i;
+                let needs_flip = byte & (1 << j) > 0;
+                let pixel = self.pixels[x][y];
+                if needs_flip {
+                    if self.pixels[x][y] == Pixel::On {
+                        collision = true;
+                    }
+                    self.pixels[x][y] = pixel.flip();
+                }
+            }
+        }
+
+        self.v[0xf] = if collision {1} else {0};
+
+        self.pc = self.pc + 2;
+    }
+
     fn jump_subroutine(&mut self, instr: u16) {
         self.stack.push(self.pc);
         self.pc = instr & 0x0fff;
@@ -104,9 +170,23 @@ impl fmt::Display for Chip8 {
     }
 }
 
+impl fmt::Display for Pixel {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let output = match *self {
+            Pixel::On => "*",
+            Pixel::Off => " ",
+        };
+        write!(f, "{}", output)
+    }
+}
+
 pub fn run(rom: Vec<u8>) {
     let mut chip8 = Chip8::initialize(rom);
     loop {
         chip8.emulate_cycle();
+
+        if chip8.draw_flag {
+            chip8.show();
+        }
     }
 }
