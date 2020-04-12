4a3566ee415dce2dc54f39b231bdbca3e504809f
Ubuntu <ubuntu@ip-172-31-22-57.us-east-2.compute.internal>
2020-04-12 17:51:39 +0000

    Added more opcodes, ncurses
    Now runs my pong rom pretty well.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,29 @@
+use ncurses;
 use rand::Rng;
+use std::char;
+use std::collections::HashMap;
 use std::fmt;
+use std::thread;
+use std::time;
+
+const KEYBOARD_MAP: [(char, usize); 16] = [
+    ('1', 1),
+    ('2', 2),
+    ('3', 3),
+    ('q', 4),
+    ('w', 5),
+    ('e', 6),
+    ('a', 7),
+    ('s', 8),
+    ('d', 9),
+    ('x', 0),
+    ('z', 0xa),
+    ('c', 0xb),
+    ('4', 0xc),
+    ('r', 0xd),
+    ('f', 0xe),
+    ('v', 0xf),
+];
 
 const INSTRUCTIONS_START: u16 = 0x200;
 const SCREEN_WIDTH: usize = 64;
@@ -48,6 +72,32 @@ impl Pixel {
     }
 }
 
+pub struct Timer {
+    start_count: u8,
+    start_instant: time::Instant,
+}
+
+impl Timer {
+    fn initialize() -> Timer {
+        Timer {
+            start_count: 0,
+            start_instant: time::Instant::now(),
+        }
+    }
+
+    fn start(&mut self, count: u8) {
+        self.start_count = count;
+        self.start_instant = time::Instant::now();
+    }
+
+    fn get_value(&self) -> u8 {
+        let now = time::Instant::now();
+        let millis = now.duration_since(self.start_instant).as_millis() as f32;
+        let ticks = millis * 60. / 1000.;
+        self.start_count.saturating_sub(ticks.floor() as u8)
+    }
+}
+
 pub struct Chip8 {
     // 4k of RAM
     ram: [u8; 4096],
@@ -64,10 +114,11 @@ pub struct Chip8 {
     // state of keys
     keys: [Key; 16],
 
-    delay_timer: u8,  // TODO: need to implement this so it counts down
-    sound_timer: u8,
+    delay_timer: Timer,
+    sound_timer: u8,  // TODO: need to implement this so it counts down
 
-    draw_flag: bool,
+    draw_queue: Vec<(u8, u8, Pixel)>,
+
 }
 
 impl Chip8 {
@@ -95,25 +146,22 @@ impl Chip8 {
             pc: INSTRUCTIONS_START,
             keys: [Key::Up; 16],
             
-            delay_timer: 0,
+            delay_timer: Timer::initialize(),
             sound_timer: 0,
             
-            draw_flag: true,
+            draw_queue: Vec::new(),
         }
     }
 
     pub fn emulate_cycle(&mut self) {
         let instr = self.fetch();
-        println!("Instruction: {}", instr);
-        if self.delay_timer > 0 {
-            self.delay_timer = self.delay_timer - 1; // TODO: not right
-        }
+        //println!("Instruction: {}", instr);
         match (instr & 0xf000) >> 12 {
             0x0 => {
                 match instr & 0x0fff {
                     0x0e0 => self.clear_screen(instr),
                     0x0ee => self.ret(instr),
-                    _ => panic!("RCA 1802 program?"),
+                    _ => panic!("RCA 1802 program? Instr: {}", instr),
                 }
             },
             0x1 => self.jump(instr),
@@ -128,7 +176,14 @@ impl Chip8 {
                     0x2 => self.reg_and(instr),
                     0x4 => self.reg_add(instr),
                     0x5 => self.reg_subtract(instr),
-                    _ => panic!("unrecognized instruction/leading 8"),
+                    0x6 => self.shift_right(instr),
+                    _ => panic!("unrecognized instruction/leading 8: {}", instr),
+                }
+            },
+            0x9 => {
+                match instr & 0x000f {
+                    0 => self.skip_if_regs_unequal(instr),
+                    _ => panic!("unrecognized instruction/leading 9: {}", instr),
                 }
             },
             0xa => self.set_index(instr),
@@ -136,8 +191,9 @@ impl Chip8 {
             0xd => self.draw_sprite(instr),
             0xe => {
                 match instr & 0x00ff {
-                    0xa1 => self.skip_if_key(instr),
-                    _ => panic!("unrecognized instruction/leading e"),
+                    0x9e => self.skip_if_key(instr),
+                    0xa1 => self.skip_if_not_key(instr),
+                    _ => panic!("unrecognized instruction/leading e: {}", instr),
                 }
             },
             0xf => {
@@ -145,13 +201,14 @@ impl Chip8 {
                     0x07 => self.get_delay_timer(instr),
                     0x15 => self.set_delay_timer(instr),
                     0x18 => self.set_sound_timer(instr),
+                    0x1e => self.add_reg_to_i(instr),
                     0x29 => self.set_char_location(instr),
                     0x33 => self.set_bcd(instr),
                     0x65 => self.reg_load(instr),
-                    _ => panic!("unrecognized instruction/leading f"),
+                    _ => panic!("unrecognized instruction/leading f: {}", instr),
                 }
             }
-            _ => panic!("unrecognized instruction!"),
+            _ => panic!("unrecognized instruction: {}", instr),
         }
     }
 
@@ -166,28 +223,34 @@ impl Chip8 {
         first_byte << 8 | second_byte
     }
 
-    fn show(&self) {
-        for y in 0..SCREEN_HEIGHT {
-            for x in 0..SCREEN_WIDTH {
-                print!("{}", self.pixels[x][y]);
-            }
-            println!("");
-        }
-    }
-
     // Opcodes
     fn add_const_to_v(&mut self, instr: u16) {
         let reg = ((instr & 0x0f00) >> 8) as usize;
         let n = (instr & 0x00ff) as u8;
 
         self.v[reg] = self.v[reg].wrapping_add(n);
-        println!("V{} == {}", reg, self.v[reg]);
+        //println!("V{} == {}", reg, self.v[reg]);
+        self.pc = self.pc + 2;
+    }
+
+    fn add_reg_to_i(&mut self, instr: u16) {
+        let reg = ((instr & 0x0f00) >> 8) as usize;
+
+        // I is actually a 12 bit value, so overflow if > 4095
+        self.i = self.i + (self.v[reg] as u16);
+        if self.i > 4095 {
+            self.v[0xf] = 1;
+        } else {
+            self.v[0xf] = 0;
+        }
+        self.i = self.i % 4096;
+
         self.pc = self.pc + 2;
     }
 
     fn clear_screen(&mut self, _instr: u16) {
+        // TODO: should add all pixels to self.draw_queue
         self.pixels = [[Pixel::Off; SCREEN_HEIGHT]; SCREEN_WIDTH];
-
         self.pc = self.pc + 2;
     }
 
@@ -201,8 +264,8 @@ impl Chip8 {
 
         let x_start = self.v[x_reg] as usize;
         let y_start = self.v[y_reg] as usize;
-        println!("x: {}, y: {}", x_start, y_start);
-        println!("n: {}", n);
+        //println!("x: {}, y: {}", x_start, y_start);
+        //println!("n: {}", n);
 
         let mem_start = self.i as usize;
 
@@ -227,6 +290,7 @@ impl Chip8 {
                         collision = true;
                     }
                     self.pixels[x][y] = pixel.flip();
+                    self.draw_queue.push((x as u8, y as u8, self.pixels[x][y]));
                 }
             }
         }
@@ -240,9 +304,9 @@ impl Chip8 {
         let reg = (instr & 0x0f00) >> 8;
         let reg = reg as usize;
 
-        self.v[reg] = self.delay_timer;
+        self.v[reg] = self.delay_timer.get_value();
 
-        println!("set register {} to {} based on delay_timer {}", reg, self.v[reg], self.delay_timer);
+        //println!("Got delay_timer: {}", self.v[reg]);
         self.pc = self.pc + 2;
     }
 
@@ -254,7 +318,7 @@ impl Chip8 {
         self.stack.push(self.pc);
         self.pc = instr & 0x0fff;
 
-        println!("jumped to subroutine at {}", self.pc);
+        //println!("jumped to subroutine at {}", self.pc);
     }
 
     fn rand(&mut self, instr: u16) {
@@ -286,13 +350,13 @@ impl Chip8 {
 
         self.v[0xf] = if overflow {1} else {0};
 
-        println!("V{} was {} and V{} was {}", reg1, self.v[reg1], reg2, self.v[reg2]);
-        println!("result should be {}", sum);
-        println!("VF is {}", self.v[0xf]);
+        //println!("V{} was {} and V{} was {}", reg1, self.v[reg1], reg2, self.v[reg2]);
+        //println!("result should be {}", sum);
+        //println!("VF is {}", self.v[0xf]);
 
         self.v[reg1] = sum;
 
-        println!("result is: {}", self.v[reg1]);
+        //println!("result is: {}", self.v[reg1]);
 
         self.pc = self.pc + 2;
     }
@@ -302,24 +366,24 @@ impl Chip8 {
 
         let result = self.v[reg1] & self.v[reg2];
 
-        println!("V{} was {} and V{} was {}", reg1, self.v[reg1], reg2, self.v[reg2]);
-        println!("result should be {}", result);
+        //println!("V{} was {} and V{} was {}", reg1, self.v[reg1], reg2, self.v[reg2]);
+        //println!("result should be {}", result);
 
         self.v[reg1] = result as u8;
 
-        println!("result is: {}", self.v[reg1]);
+        //println!("result is: {}", self.v[reg1]);
 
         self.pc = self.pc + 2;
     }
 
     fn reg_load(&mut self, instr: u16) {
         let count = ((instr & 0x0f00) >> 8) + 1;
-        println!("count: {}", count);
-        println!("contents of &I: {} {} {}", self.ram[self.i as usize], self.ram[self.i as usize + 1], self.ram[self.i as usize + 2]);
+        //println!("count: {}", count);
+        //println!("contents of &I: {} {} {}", self.ram[self.i as usize], self.ram[self.i as usize + 1], self.ram[self.i as usize + 2]);
         for reg in 0..count {
             let mem_location = (self.i + reg) as usize;
             self.v[reg as usize] = self.ram[mem_location];
-            println!("Stored {} in V{}", self.v[reg as usize], reg);
+            //println!("Stored {} in V{}", self.v[reg as usize], reg);
         }
 
         self.pc = self.pc + 2;
@@ -340,15 +404,17 @@ impl Chip8 {
 
         let (sum, overflow) = val1.overflowing_sub(val2);
 
-        self.v[0xf] = if overflow {1} else {0};
+        self.v[0xf] = if overflow {0} else {1};
 
-        println!("V{} was {} and V{} was {}", reg1, self.v[reg1], reg2, self.v[reg2]);
-        println!("result should be {}", sum);
-        println!("VF is {}", self.v[0xf]);
+        //panic!("subtracting {} from {} with result {}, overflow {}", val2, val1, sum, overflow);
+
+        //println!("V{} was {} and V{} was {}", reg1, self.v[reg1], reg2, self.v[reg2]);
+        //println!("result should be {}", sum);
+        //println!("VF is {}", self.v[0xf]);
 
         self.v[reg1] = sum;
 
-        println!("result is: {}", self.v[reg1]);
+        //println!("result is: {}", self.v[reg1]);
 
         self.pc = self.pc + 2;
     }
@@ -359,7 +425,7 @@ impl Chip8 {
         });
 
         self.pc = addr + 2;
-        println!("returned from subroutine to {}", self.pc);
+        //println!("returned from subroutine to {}", self.pc);
     }
 
     fn set_bcd(&mut self, instr: u16) {
@@ -369,7 +435,7 @@ impl Chip8 {
         let hundreds = val / 100;
         let tens = (val - 100 * hundreds) / 10;
         let ones = val - 100 * hundreds - 10 * tens;
-        println!("val: {}; hundreds: {}, tens: {}, ones: {}", val, hundreds, tens, ones);
+        //println!("val: {}; hundreds: {}, tens: {}, ones: {}", val, hundreds, tens, ones);
 
         let start = self.i as usize;
         self.ram[start] = hundreds;
@@ -391,9 +457,9 @@ impl Chip8 {
         let reg = (instr & 0x0f00) >> 8;
         let reg = reg as usize;
 
-        self.delay_timer = self.v[reg];
+        self.delay_timer.start(self.v[reg]);
 
-        println!("set delay_timer to {} based on register {}", self.delay_timer, reg);
+        //println!("set delay_timer to {} based on register {}", self.v[reg], reg);
         
         self.pc = self.pc + 2;
     }
@@ -403,7 +469,7 @@ impl Chip8 {
         let value = instr & 0x0fff;
         self.i = value;
 
-        println!("set I to {}", self.i);
+        //println!("set I to {}", self.i);
 
         self.pc = self.pc + 2;
     }
@@ -416,7 +482,7 @@ impl Chip8 {
 
         self.v[reg] = value;
 
-        println!("Set V{} to {}", reg, self.v[reg]);
+        //println!("Set V{} to {}", reg, self.v[reg]);
 
         self.pc = self.pc + 2;
     }
@@ -424,7 +490,17 @@ impl Chip8 {
     fn set_sound_timer(&mut self, instr: u16) {
         let reg = ((instr & 0x0f00) >> 8) as usize;
         self.sound_timer = self.v[reg];
-        println!("setting sound_timer to {}", self.sound_timer);
+        //println!("setting sound_timer to {}", self.sound_timer);
+
+        self.pc = self.pc + 2;
+    }
+
+    fn shift_right(&mut self, instr: u16) {
+        let reg = ((instr & 0x0f00) >> 8) as usize;
+        let val = self.v[reg];
+
+        self.v[0xf] = 1 & val;
+        self.v[reg] = val >> 1;
 
         self.pc = self.pc + 2;
     }
@@ -434,8 +510,14 @@ impl Chip8 {
         let n = (instr & 0x00ff) as u8;
 
         let incr = if self.v[reg] == n {4} else {2};
-        println!("Incrementing by {}", incr);
+        //println!("Incrementing by {}", incr);
         self.pc = self.pc + incr;
+    }
+
+    fn skip_if_regs_unequal(&mut self, instr: u16) {
+       let (reg1, reg2) = self.reg_get_for_math(instr); 
+       let incr = if self.v[reg1] != self.v[reg2] {4} else {2};
+       self.pc = self.pc + incr;
     }
 
     fn skip_if_unequal(&mut self, instr: u16) {
@@ -449,13 +531,33 @@ impl Chip8 {
         let reg = (instr & 0x0f00) >> 8;
         let reg = reg as usize;
 
-        let key_index = self.v[reg] as usize;
-        let incr = match self.keys[key_index] {
+        let incr = match self.test_key(self.v[reg]) {
             Key::Up => 2,
             Key::Down => 4,
         };
 
         self.pc = self.pc + incr;
+    }
+
+    fn skip_if_not_key(&mut self, instr: u16) {
+        let reg = (instr & 0x0f00) >> 8;
+        let reg = reg as usize;
+
+        let incr = match self.test_key(self.v[reg]) {
+            Key::Up => 4,
+            Key::Down => 2,
+        };
+        self.pc = self.pc + incr;
+    }
+
+    fn test_key(&mut self, key_index: u8) -> Key {
+        // This isn't right - in the Chip8, keys don't get "reset" when read. However, ncurses
+        // doesn't detect "key up" events, so this seems like a good place to set they key back to
+        // up.
+        let key_index = key_index as usize;
+        let key = self.keys[key_index].clone();
+        self.keys[key_index] = Key::Up;
+        key
     }
 }
 
@@ -476,12 +578,41 @@ impl fmt::Display for Pixel {
 }
 
 pub fn run(rom: Vec<u8>) {
+    let keyboard: HashMap<char, usize> = KEYBOARD_MAP.iter().cloned().collect();
+
+    ncurses::initscr();
+    ncurses::raw();
+    ncurses::curs_set(ncurses::CURSOR_VISIBILITY::CURSOR_INVISIBLE);
+    ncurses::nodelay(ncurses::stdscr(), true);
+    ncurses::noecho();
+
     let mut chip8 = Chip8::initialize(rom);
     loop {
+        let ch = char::from_u32(ncurses::getch() as u32);
+
+        if let Some(k) = ch {
+            if let Some(key) = keyboard.get(&k) {
+                chip8.keys[*key] = Key::Down;
+            }
+        }
+
         chip8.emulate_cycle();
 
-        if chip8.draw_flag {
-            chip8.show();
+
+        for item in chip8.draw_queue.iter() {
+            let (x, y, pixel) = item;
+
+            let ch = match pixel {
+                Pixel::On => '#',
+                Pixel::Off => ' ',
+            };
+            ncurses::mvaddch(*y as i32, *x as i32, ch as ncurses::chtype);
         }
+        ncurses::refresh();
+        chip8.draw_queue.clear();
+
+
+        let duration = time::Duration::from_millis(2);
+        thread::sleep(duration);  // TODO: fix the timing
     }
 }
